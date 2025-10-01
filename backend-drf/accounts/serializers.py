6fbeb95c430eeb1d9056a25d493fad672f7aae9b
Shivang3000 <shivangsingh260@gmail.com>
2025-10-01 21:00:45 +0530

    registration backend/api

---
+++ backend-drf/accounts/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer (serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        min_length=3,
+        style={'input_type':'password'}
+        )
+
+    class Meta:
+        model = User
+        fields = ['username','email','password']
+
+        def create(self,validator_data):
+            user = User.objects.create_user(
+                validator_data['username'],
+                validator_data['email'],
+                validator_data['password']
+            )
+
+            return user
